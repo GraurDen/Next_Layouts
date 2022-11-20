ca06fa1b8192b9d196522f223cc840cdc9d2e603
Evgeniy Grinko <grinko.evdev@gmail.com>
2022-11-20 20:40:21 +0300

    Hello world

--- components/layouts/adminLayout/admin.tsx
+++ components/layouts/adminLayout/admin.tsx
@@ -4,6 +4,7 @@ import { ReactElement } from "react";
 const AdminLayout = ({ children }: { children: ReactElement }) => {
   return (
     <div style={{ textAlign: "center" }}>
+      <h1>Hello world !!!</h1>
       <Header />
       {children}
     </div>
