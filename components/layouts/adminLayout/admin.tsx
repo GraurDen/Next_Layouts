70caea211e2d4a8d8d0ee68ca16e8d15170b7e17
Evgeniy Grinko <grinko.evdev@gmail.com>
2022-11-20 20:34:21 +0300

    Revert "Merge pull request #8 from GraurDen/testAfterHardReset"
    This reverts commit 1ba439f780046d25a78d2f441905206b458f184c, reversing
    changes made to 36c270de8e88011d7b407559547ec149904765bb.

--- components/layouts/adminLayout/admin.tsx
+++ components/layouts/adminLayout/admin.tsx
@@ -4,8 +4,6 @@ import { ReactElement } from "react";
 const AdminLayout = ({ children }: { children: ReactElement }) => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Test 1 with revert</h1>
-      <h1>Test 2 with revert</h1>
       <Header />
       {children}
     </div>
