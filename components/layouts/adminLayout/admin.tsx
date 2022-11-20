a65658aff6ab047fa58ac014ece9e80a0465eb6d
Evgeniy Grinko <grinko.evdev@gmail.com>
2022-11-20 20:28:16 +0300

    Revert test 2

--- components/layouts/adminLayout/admin.tsx
+++ components/layouts/adminLayout/admin.tsx
@@ -4,7 +4,8 @@ import { ReactElement } from "react";
 const AdminLayout = ({ children }: { children: ReactElement }) => {
   return (
     <div style={{ textAlign: "center" }}>
-      <div>Test 1 with revert</div>
+      <h1>Test 1 with revert</h1>
+      <h1>Test 2 with revert</h1>
       <Header />
       {children}
     </div>
