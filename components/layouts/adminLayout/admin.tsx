04e03ff2263e0a90926510aef99032821e2b019b
Evgeniy Grinko <grinko.evdev@gmail.com>
2022-11-20 15:37:16 +0300

    Revert "Merge pull request #3 from GraurDen/changes_2"
    This reverts commit 97fe43d56edd1fdc17bda5192e7e25762a0890dd, reversing
    changes made to 1f5d766046468bc6a9880fbeb111e749171093a8.

--- components/layouts/adminLayout/admin.tsx
+++ components/layouts/adminLayout/admin.tsx
@@ -3,15 +3,12 @@ import { ReactElement } from "react";
 
 const AdminLayout = ({ children }: { children: ReactElement }) => {
   return (
-    <div style={{ textAlign: "center" }}>
+    <>
       <Header />
       <h1 style={{ color: "greenyellow" }}>The commit i'm going back to</h1>
-      <h2 style={{ color: "burlywood" }}>Test change that will be revert</h2>
-      <h3 style={{ color: "blueviolet" }}>
-        Test with secont change that will be revert to
-      </h3>
+      <h2 style={{ color: "bisque" }}>Test change that will be revert</h2>
       {children}
-    </div>
+    </>
   );
 };
 
